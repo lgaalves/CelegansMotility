d55db87c4bfc0789ea3d7acb8a072c701e0561de
Luiz Alves <gustavoandradealves@gmail.com>
2017-07-07 12:58:05 -0300

    All code running with new analysis

--- python/msd_bootstrapping.py
+++ python/msd_bootstrapping.py
@@ -14,23 +14,38 @@ import pathlib
 from scipy import optimize
 import random
 import warnings
+
+
 warnings.filterwarnings('ignore')
 
 # Locals
 from modules.functions import *
 
+
 def main():
-    data_plot=[]
     for age in [1,2,3,4,5,6]:
         df_x,df_y=read_dataset("age",label=age)
+        data_plot=[]
         slope_bootstrapping=[]
         for i in range(0,100):
-            print(age,i)
             slope=slope_random_sample(df_x,df_y)
+            print(age,i,slope)
             slope_bootstrapping.append(slope)
         data_plot.append(slope_bootstrapping)
         data_plot = pd.DataFrame(data_plot)
         data_plot.to_csv('../results/msd_bootstrapping_{}.csv'.format(age), index=False, header=False)
+
+    for t in [15,20,25]:
+        df_x,df_y=read_dataset("temperature",label=t)
+        data_plot=[]
+        slope_bootstrapping=[]
+        for i in range(0,100):
+            slope=slope_random_sample(df_x,df_y)
+            print(t,i,slope)
+            slope_bootstrapping.append(slope)
+        data_plot.append(slope_bootstrapping)
+        data_plot = pd.DataFrame(data_plot)
+        data_plot.to_csv('../results/msd_bootstrapping_{}.csv'.format(t), index=False, header=False)
 
 
 if __name__ == "__main__":
